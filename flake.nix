9242b4175dacd4bc6c22e40457988c0969e1619c
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2026-01-30 10:10:26 -0500

    nixfmt

--- flake.nix
+++ flake.nix
@@ -1,16 +1,18 @@
 {
   description = "Geant4 development shell";
 
-  outputs = { self }: {
-    templates = {
-      default = {
-        path = ./dev-shell;
-        description = "Nix development shell with Geant4 and ROOT";
-        welcomeText = ''
-          Run `nix develop` to enter the development environment.
-        '';
+  outputs =
+    { self }:
+    {
+      templates = {
+        default = {
+          path = ./dev-shell;
+          description = "Nix development shell with Geant4 and ROOT";
+          welcomeText = ''
+            Run `nix develop` to enter the development environment.
+          '';
+        };
+        dev-shell = self.templates.default;
       };
-      dev-shell = self.templates.default;
     };
-  };
 }
